699526351d7b420d6c25f3bc3eb78de753144ac6
JeanStkng <jjg.castillo@yavirac.edu.ec>
2020-08-18 03:29:54 -0500

    combinado con cambios del inge y nuestros

--- routes/api/job_board/api.php
+++ routes/api/job_board/api.php
@@ -14,4 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Rutas ofertas
+Route::group(['prefix' => 'offers'], function () {
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('', 'OfferController@getAllOffers'); // Trae todas las ofertas.
+        Route::get('/opportunities', 'OfferController@getOffers'); // Trae todas las ofertas con filtros
 
+        Route::get('/totalOffers', 'OfferController@getTotalOffers'); // Cuenta todas las ofertas.
+        Route::post('/filter', 'OfferController@filterOffers'); // Filtra las ofertas segun el buscador.
+
+        // Route::get('/professionals', 'OfferController@getAppliedProfessionals');
+        // Route::get('/opportunities/validateAppliedOffer', 'OfferController@validateAppliedOffer');
+        // Route::post('/opportunities/apply', 'OfferController@applyOffer');
+
+        // Estos metodo de traer todos los profesionales y companies no nos corresponde como tal,
+        // Preguntar al ing
+        Route::get('/totalProfessionals', 'ProfessionalController@getTotalProfessionals'); 
+        Route::get('/totalCompanies', 'CompanyController@getTotalCompanies');
+
+        // Route::get('/opportunities/filter', 'OfferController@filterOffersFields');
+
+    });
+});
