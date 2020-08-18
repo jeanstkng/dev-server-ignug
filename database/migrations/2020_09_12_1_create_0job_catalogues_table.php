3449718c906c3e223c87192d7ea178ea9d8c0cdc
JeanStkng <jjg.castillo@yavirac.edu.ec>
2020-08-18 14:16:29 -0500

    Combinadas migraciones

--- database/migrations/2020_09_12_1_create_0job_catalogues_table.php
+++ database/migrations/2020_09_12_1_create_0job_catalogues_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJobCataloguesTable extends Migration
+class Create0JobCataloguesTable extends Migration
 {
     /**
      * Run the migrations.
